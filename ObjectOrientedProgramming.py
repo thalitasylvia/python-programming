7e53d7cfc9632a81cc5e543106a4de21000ee2e5
Thalita <thalita.sylvia@gmail.com>
2024-01-02 18:30:29 -0300

    Class constructor

---
+++ ObjectOrientedProgramming.py
@@ -0,0 +1,9 @@
+
+def simple_constructor():
+    class Stack:  # Defining the Stack class.
+        def __init__(self):  # Defining the constructor function.
+            print("Hi!")
+
+
+    stack_object = Stack()  # Instantiating the object.
+# OUTPUT: Hi!
