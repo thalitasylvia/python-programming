a168c2f1ca706a51b33512c669d6900a79ad64ca
Thalita <thalita.sylvia@gmail.com>
2024-01-03 20:54:49 -0300

    Object property

--- ObjectOrientedProgramming.py
+++ ObjectOrientedProgramming.py
@@ -7,3 +7,15 @@ def simple_constructor():
 
     stack_object = Stack()  # Instantiating the object.
 # OUTPUT: Hi!
+
+
+
+def object_w_property():
+    class Stack:
+        def __init__(self):
+            self.stack_list = []
+
+
+    stack_object = Stack()
+    print(len(stack_object.stack_list))
+# OUTPUT: 0
