3c3c74dab3d8e6333c37be44e443b2b60f2af17e
Thalita <thalita.sylvia@gmail.com>
2024-01-04 08:57:30 -0300

    Private property

--- ObjectOrientedProgramming.py
+++ ObjectOrientedProgramming.py
@@ -19,3 +19,15 @@ def object_w_property():
     stack_object = Stack()
     print(len(stack_object.stack_list))
 # OUTPUT: 0
+
+
+
+def private_property():
+    class Stack:
+        def __init__(self):
+            self.__stack_list = []
+
+
+    stack_object = Stack()
+    print(len(stack_object.__stack_list))
+# OUTPUT: AttributeError: 'Stack' object has no attribute '__stack_list'
