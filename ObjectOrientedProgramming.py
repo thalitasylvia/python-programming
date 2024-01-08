a31f89f6189c35063eca93731d470a352598cf06
Thalita <thalita.sylvia@gmail.com>
2024-01-07 21:12:47 -0300

    Independent stack objects

--- ObjectOrientedProgramming.py
+++ ObjectOrientedProgramming.py
@@ -88,3 +88,30 @@ def independent_stack_objects():
 
     print(stack_object_2.pop())
 # OUTPUT: 3
+
+
+
+def independent_stacks():
+    class Stack:
+        def __init__(self):
+            self.__stack_list = []
+
+        def push(self, val):
+            self.__stack_list.append(val)
+
+        def pop(self):
+            val = self.__stack_list[-1]
+            del self.__stack_list[-1]
+            return val
+
+
+    little_stack = Stack()
+    another_stack = Stack()
+    funny_stack = Stack()
+
+    little_stack.push(1)
+    another_stack.push(little_stack.pop() + 1)
+    funny_stack.push(another_stack.pop() - 2)
+
+    print(funny_stack.pop())
+# OUTPUT: 0
